ed15423a7974228a1827623bf5453ea08ff4ac5d
MykolaKovblyuk <mkvblyuk@gmail.com>
2024-05-07 13:54:19 +0300

    add events and listeners

---
+++ src/app/Listeners/SendNotification.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Contracts\Events\ShouldHandleEventsAfterCommit;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SendNotification implements ShouldHandleEventsAfterCommit
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(object $event): void
+    {
+        // TODO send notifications
+    }
+}
