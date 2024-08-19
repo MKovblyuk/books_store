760cf68a9df89a41143b06609b1e1e2149e7fdf3
MKovblyuk <mkvblyuk@gmail.com>
2024-08-19 20:31:34 +0300

    add select component

---
+++ frontend/src/components/ui/UISelect.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+const props = defineProps(['id', 'items', 'defaultTitle']);
+
+</script>
+
+<template>
+    <select 
+        class="form-select" 
+        :aria-label="id" 
+        :id="id" 
+    >
+        <option value='' selected>{{ defaultTitle }}</option>
+        <option 
+            v-for="item in items" 
+            :value="item.id"     
+            :key="item.id"
+        >
+            {{ item.name }}
+        </option>
+    </select>
+</template>
---
+++ frontend/src/components/ui/UISelectWithLabel.vue
@@ -0,0 +1,27 @@
+<script setup>
+import UISelect from './UISelect.vue';
+
+const props = defineProps(['id', 'items', 'defaultTitle', 'labelText', 'required', 'disabled']);
+
+</script>
+
+<template>
+    <div>
+        <label :for="id">
+            {{ labelText }} <span v-if="required" class="required_asterisk">*</span>
+        </label>
+        <UISelect 
+            :id
+            :items
+            :default-title="defaultTitle"
+            :disabled
+        />
+    </div>
+
+</template>
+
+<style scoped>
+    .required_asterisk {
+        color: red;
+    }
+</style>
