52f92b31d68934446d667215a60b8846b01a0ee2
MKovblyuk <mkvblyuk@gmail.com>
2024-09-12 15:16:23 +0300

    add adding reviews

--- frontend/src/components/reviews/AddReviewForm.vue
+++ frontend/src/components/reviews/AddReviewForm.vue
@@ -2,6 +2,7 @@
 import { useReviewStore } from '@/stores/reviewStore';
 import axios from 'axios';
 import { ref } from 'vue';
+import RatingBar from './RatingBar.vue';
 
 const review = ref('');
 const rating = ref(1);
@@ -27,15 +28,19 @@ function addReviewHandler() {
 </script>
 
 <template>
-        <div class="form-floating p-2">
-            <textarea 
-                class="form-control" 
-                placeholder="Leave a comment here" 
-                id="floatingTextarea"
-                v-model="review"
-            ></textarea>
-            <label for="floatingTextarea">Add Review</label>
+        <div class="p-2">
+            <RatingBar v-model="rating"/>
 
+            <div class="form-floating mt-2">
+                <textarea 
+                    class="form-control" 
+                    placeholder="Leave a comment here" 
+                    id="floatingTextarea"
+                    v-model="review"
+                ></textarea>
+                <label for="floatingTextarea">Add Review</label>
+            </div>
+    
             <button 
                 class="btn btn-primary mt-1"
                 @click="addReviewHandler"
---
+++ frontend/src/components/reviews/RatingBar.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { ref } from 'vue';
+
+const iconPath = ref('/src/assets/icons/rating-star.svg');
+const selectedIconPath = ref('/src/assets/icons/rating-star-yellow.svg')
+
+const rating = defineModel();
+
+</script>
+
+<template>
+    <div>
+        Rating: 
+        <span class="star-rating">
+            <template>
+
+            </template>
+            <label for="rate-1"> 
+                <img :src="rating >= 1 ? selectedIconPath : iconPath">
+            </label>
+            <input type="radio" name="rating" id="rate-1" value="1" v-model="rating">
+            <label for="rate-2">
+                <img :src="rating >= 2 ? selectedIconPath : iconPath">
+            </label>
+            <input type="radio" name="rating" id="rate-2" value="2" v-model="rating">
+            <label for="rate-3">
+                <img :src="rating >= 3 ? selectedIconPath : iconPath">
+            </label>
+            <input type="radio" name="rating" id="rate-3" value="3" v-model="rating">
+            <label for="rate-4">
+                <img :src="rating >= 4 ? selectedIconPath : iconPath">
+            </label>
+            <input type="radio" name="rating" id="rate-4" value="4" v-model="rating">
+            <label for="rate-5">
+                <img :src="rating >= 5 ? selectedIconPath : iconPath">
+            </label>
+            <input type="radio" name="rating" id="rate-5" value="5" v-model="rating">
+	    </span>
+    </div>
+</template>
+
+<style scoped>
+
+.star-rating {
+	white-space: nowrap;
+}
+.star-rating [type="radio"] {
+	appearance: none;
+}
+.star-rating img {
+	height: 1.4rem;
+    width: 1.4rem;
+}
+
+.star-rating label:is(:hover, :has(~ :hover)) img {
+	transform: scale(1.35);
+}
+
+</style>
