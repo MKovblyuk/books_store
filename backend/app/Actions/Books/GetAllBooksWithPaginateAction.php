1068e4e99de49fb40eadbe733656eeed50946d6d
MKovblyuk <mkvblyuk@gmail.com>
2024-09-13 17:58:46 +0300

    added filtering by category including subcategories

--- backend/app/Actions/Books/GetAllBooksWithPaginateAction.php
+++ backend/app/Actions/Books/GetAllBooksWithPaginateAction.php
@@ -3,18 +3,13 @@
 namespace App\Actions\Books;
 
 use App\Filters\FilterBooksByAuthors;
+use App\Filters\FilterBooksByCategoryWithChild;
 use App\Filters\FilterBooksByFormats;
 use App\Filters\FilterBooksByPrice;
-use App\Helpers\DirectoryNameGenerator;
-use App\Models\V1\Books\AudioFormat;
-use App\Models\V1\Books\Author;
 use App\Models\V1\Books\Book;
-use App\Models\V1\Books\ElectronicFormat;
-use App\Models\V1\Books\PaperFormat;
 use App\Sorts\SortBooksByLikes;
 use App\Sorts\SortBooksByPrice;
 use App\Sorts\SortBooksBySellingCount;
-use Illuminate\Support\Facades\DB;
 use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\AllowedSort;
 use Spatie\QueryBuilder\QueryBuilder;
@@ -23,14 +18,13 @@ class GetAllBooksWithPaginateAction
 {
     public function execute(int $per_page)
     {
-        // $per_page = request()->get('per_page', 10);
-
         return QueryBuilder::for(Book::class)
             ->allowedFilters([
                 AllowedFilter::exact('id'),
                 AllowedFilter::custom('author_id', new FilterBooksByAuthors()),
                 AllowedFilter::custom('format', new FilterBooksByFormats()),
                 AllowedFilter::custom('price_range', new FilterBooksByPrice()),
+                AllowedFilter::custom('category_with_children', new FilterBooksByCategoryWithChild()),
                 AllowedFilter::exact('publisher_id'),
                 AllowedFilter::exact('category_id'),
                 'name',
---
+++ backend/app/Filters/FilterBooksByCategoryWithChild.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Filters;
+
+use App\Models\V1\Books\Category;
+use Illuminate\Database\Eloquent\Builder;
+use Spatie\QueryBuilder\Filters\Filter;
+
+class FilterBooksByCategoryWithChild implements Filter {
+    public function __invoke(Builder $query, $value, string $property)
+    {
+        $childCategoriesIds = Category::find($value)->descendants()->pluck('id');
+        $childCategoriesIds->push($value);
+
+        $query->whereIn('category_id', $childCategoriesIds);
+    }
+}
