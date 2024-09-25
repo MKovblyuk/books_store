169767b2ab8da8fafeb5fddfa64b6eab68f458bb
MKovblyuk <mkvblyuk@gmail.com>
2024-09-25 20:10:42 +0300

    added retrieving functionality

--- backend/app/Http/Controllers/Api/V1/Orders/PaymentMethodController.php
+++ backend/app/Http/Controllers/Api/V1/Orders/PaymentMethodController.php
@@ -3,14 +3,33 @@
 namespace App\Http\Controllers\Api\V1\Orders;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\Orders\PaymentMethodCollection;
 use App\Models\V1\Orders\PaymentMethod;
-use Faker\Provider\ar_EG\Payment;
-use Illuminate\Http\Request;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class PaymentMethodController extends Controller
 {
     public function index()
-    {
-        return PaymentMethod::all();
+    {   
+        $paymentMethods = QueryBuilder::for(PaymentMethod::class)
+            ->allowedFields([
+                'id', 
+                'method', 
+                'name'
+            ])
+            ->allowedFilters([
+                AllowedFilter::exact('id'), 
+                'method', 
+                'name'
+            ])
+            ->allowedSorts([
+                'id',
+                'method',
+                'name'
+            ])
+            ->get();
+
+        return new PaymentMethodCollection($paymentMethods);
     }
 }
