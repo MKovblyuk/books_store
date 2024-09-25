e78646086d96249ef9e972353dd89b725654559d
MKovblyuk <mkvblyuk@gmail.com>
2024-09-25 19:51:45 +0300

    add payment method resource

---
+++ backend/app/Http/Resources/V1/Orders/PaymentMethodResource.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Resources\V1\Orders;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PaymentMethodResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        if (isset($request->fields)) {
+            return $this->resourceWithSelectedFields($request);
+        }
+
+        return [
+            'id' => $this->id,
+            'method' => $this->method,
+            'name' => $this->name,
+        ];
+    }
+
+    public function resourceWithSelectedFields(Request $request): array
+    {
+        $fields = explode(',', $request->fields['payment_methods']);
+
+        return [
+            $this->mergeWhen(in_array('id', $fields), [
+                'id' => $this->id,
+            ]),
+            $this->mergeWhen(in_array('method', $fields), [
+                'method' => $this->method,
+            ]),
+            $this->mergeWhen(in_array('name', $fields), [
+                'name' => $this->name,
+            ]),
+        ];
+    }
+}
