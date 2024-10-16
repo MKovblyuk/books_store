899ed1320297ba347bf591f357e85aae2f36d8f2
MKovblyuk <mkvblyuk@gmail.com>
2024-10-16 15:24:16 +0300

    add postal code

--- backend/database/migrations/2024_02_20_090033_create_districts_table.php
+++ backend/database/migrations/2024_02_20_090033_create_districts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('districts', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('postal_code');
 
             $table->foreignId('region_id')
                 ->constrained()
--- backend/database/migrations/2024_09_23_125700_create_settlements_table.php
+++ backend/database/migrations/2024_09_23_125700_create_settlements_table.php
@@ -11,6 +11,8 @@ return new class extends Migration
         Schema::create('settlements', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('postal_code');
+
             $table->foreignId('district_id')
                 ->constrained()
                 ->cascadeOnUpdate()
