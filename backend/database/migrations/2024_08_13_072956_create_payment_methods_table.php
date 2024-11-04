be192aab8aea030c11e3b4eb3748f6fa8f7c3db8
MKovblyuk <mkvblyuk@gmail.com>
2024-11-04 11:57:02 +0200

    change migrations

--- backend/database/migrations/2024_08_13_072956_create_payment_methods_table.php
+++ backend/database/migrations/2024_08_13_072956_create_payment_methods_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('payment_methods', function (Blueprint $table) {
             $table->id();
             $table->enum('method', PaymentMethods::values())->unique();
-            $table->string('name')->nullable();
+            $table->string('name')->unique();
         });
     }
 
