738eca77ff879f6b7be7a5f38a8700e2c13b1292
MKovblyuk <mkvblyuk@gmail.com>
2024-11-26 13:00:12 +0200

    handle exceptions

--- backend/app/Actions/Books/UpdateBookAction.php
+++ backend/app/Actions/Books/UpdateBookAction.php
@@ -2,8 +2,10 @@
 
 namespace App\Actions\Books;
 
+use App\Listeners\Books\DeleteBookCoverImage;
 use App\Models\V1\Books\Author;
 use App\Models\V1\Books\Book;
+use Exception;
 use Illuminate\Support\Facades\DB;
 
 class UpdateBookAction 
@@ -13,6 +15,7 @@ class UpdateBookAction
         private UpdateOrCreateElectronicFormatAction $updateOrCreateElectronicFormatAction,
         private UpdateOrCreateAudioFormatAction $updateOrCreateAudioFormatAction,
         private UpdateBookCoverImageAction $updateBookCoverImageAction,
+        private DeleteBookCoverImageAction $deleteBookCoverImageAction
     )
     {}
 
@@ -24,20 +27,35 @@ class UpdateBookAction
             if (isset($attributes['authors_ids'])) {
                 $book->authors()->sync(Author::find($attributes['authors_ids']));
             }
-            if (isset($attributes['formats']['paper'])) {
-                $this->updateOrCreatePaperFormatAction->execute($book, $attributes['formats']['paper']);
-            }
-            if (isset($attributes['formats']['electronic'])) {
-                $this->updateOrCreateElectronicFormatAction->execute($book, $attributes['formats']['electronic']);
-            }
-            if (isset($attributes['formats']['audio'])) {
-                $this->updateOrCreateAudioFormatAction->execute($book, $attributes['formats']['audio']);
-            }
-            if (isset($attributes['cover_image'])) {
-                $this->updateBookCoverImageAction->execute($book, $attributes['cover_image']);
-            }
 
-            return true;
+            try {
+                if (isset($attributes['formats']['paper'])) {
+                    $this->updateOrCreatePaperFormatAction->execute($book, $attributes['formats']['paper']);
+                }
+                if (isset($attributes['formats']['electronic'])) {
+                    $this->updateOrCreateElectronicFormatAction->execute($book, $attributes['formats']['electronic']);
+                }
+                if (isset($attributes['formats']['audio'])) {
+                    $this->updateOrCreateAudioFormatAction->execute($book, $attributes['formats']['audio']);
+                }
+                if (isset($attributes['cover_image'])) {
+                    $this->updateBookCoverImageAction->execute($book, $attributes['cover_image']);
+                }
+
+                return true;
+            } catch(Exception $e) {
+                if (isset($attributes['formats']['audio'])) {
+                    $book->audioFormat->getFileStorageService()->delete();
+                }
+                if (isset($attributes['formats']['electronic'])) {
+                    $book->electronicFormat->getFileStorageService()->delete();
+                }
+                if (isset($attributes['cover_image'])) {
+                    $this->deleteBookCoverImageAction->execute($book);
+                }
+
+                throw $e;
+            }
         });
     }
 }
