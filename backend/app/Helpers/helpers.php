1e4f4ce44769e31d043dd4c361d84d4c820fee8e
MKovblyuk <mkvblyuk@gmail.com>
2024-12-16 21:17:42 +0200

    added memory usage helper

--- backend/app/Helpers/helpers.php
+++ backend/app/Helpers/helpers.php
@@ -6,3 +6,12 @@ if (!function_exists('notFoundJsonResponse')) {
         return response()->json(['message' => 'Not Found Resource'], 404);
     }
 }
+
+if (!function_exists('getVariableMemoryUsage')) {
+    function getVariableMemoryUsage($var)
+    {
+        $startMemory = memory_get_usage();
+        $tmp = unserialize(serialize($var)); 
+        return memory_get_usage() - $startMemory;
+    }
+}
