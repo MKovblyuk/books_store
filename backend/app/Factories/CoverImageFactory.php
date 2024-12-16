65d0cfb3aa6f3ba0cc5cb1ff698cd755d68ceba1
MKovblyuk <mkvblyuk@gmail.com>
2024-12-16 21:18:23 +0200

    fix cover image factory

--- backend/app/Factories/CoverImageFactory.php
+++ backend/app/Factories/CoverImageFactory.php
@@ -2,19 +2,23 @@
 namespace App\Factories;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
+
 class CoverImageFactory
 {
     private $files = []; 
     private $createdLinks = [];
+
     public function __construct()
     {
         $this->files = Storage::disk('public')->allFiles('seeding_files/cover_images/');    
     }
+
     public function create(): UploadedFile
     {
         $filePath = $this->files[array_rand($this->files)];
         return new UploadedFile(public_path('storage/' . $filePath), basename($filePath));
     }
+
     /**
      * @return string Path to link on image
      */
@@ -23,9 +27,12 @@ class CoverImageFactory
         $filePath = Storage::disk('public')->path($this->files[array_rand($this->files)]);
         $pathToLink = uniqid('cover_image_');
         $this->createdLinks[] = $pathToLink;
-        link($filePath, Storage::disk('preview_fragments')->path($pathToLink));
+
+        symlink($filePath, Storage::disk('preview_fragments')->path($pathToLink));
+
         return $pathToLink;
     }
+
     /**
      * Remove all created files
      */
@@ -33,5 +40,10 @@ class CoverImageFactory
     {
         Storage::disk('preview_fragments')->delete($this->createdLinks);
         $this->createdLinks = [];
+    }
+
+    private function changePermissions(): void
+    {
+        shell_exec('chown -R www-data:www-data storage/app/public/preview_fragments');
     }
 }
