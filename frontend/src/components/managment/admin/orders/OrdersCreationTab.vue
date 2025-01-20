30ecb50f7da5525b92b46ac6c0005f070c6d6271
MKovblyuk <mkvblyuk@gmail.com>
2025-01-20 14:30:20 +0200

    fixed sales chart

--- frontend/src/components/managment/admin/orders/OrdersCreationTab.vue
+++ frontend/src/components/managment/admin/orders/OrdersCreationTab.vue
@@ -1,22 +1,38 @@
 <script setup>
 import SimpleLineChart from '@/components/widgets/charts/SimpleLineChart.vue';
+import SwitchButtonsGroup from '@/components/widgets/SwitchButtonsGroup.vue';
+import { useStatisticFormatter } from '@/composables/statisticFormatter';
 import axios from 'axios';
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 
 const labels = ref([]);
 const data = ref([]);
+const selectedYear = ref((new Date()).getFullYear());
 
 onMounted(() => {
     fetchOrdersCreationInfo();
 })
 
+watch(selectedYear,() => fetchOrdersCreationInfo());
+
 async function fetchOrdersCreationInfo() {
-    const response = await axios.get('orders/creationInfo');
+    const response = await axios.get('orders/creationInfo', {
+        params: {
+            year: selectedYear.value
+        }
+    });
 
     if (response.status === 200) {
-        labels.value = response.data.data.map(item => item.creationDate);
-        data.value = response.data.data.map(item => item.ordersCount);
+        setLabels();
+        data.value = useStatisticFormatter(selectedYear.value)
+            .getData(response.data.data[0], (item) => item[0]?.ordersCount);
     }
+}
+
+function setLabels() {
+    labels.value = useStatisticFormatter(selectedYear.value)
+        .getMonthsNumbers(selectedYear.value)
+        .map(item => item + '/' + selectedYear.value);
 }
 
 </script>
@@ -29,6 +45,10 @@ async function fetchOrdersCreationInfo() {
             title="Sales Chart"
             y-title="Sales count"
             data-set-label="Sales"
+        />
+        <SwitchButtonsGroup 
+            class="mt-2 d-flex justify-content-center"
+            v-model="selectedYear"
         />
     </div>
 </template>
